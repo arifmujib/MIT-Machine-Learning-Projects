8b9e7152ed731af3ceb8702f9232b0fda0357d53
Mujibur <arif.mujibur@gmail.com>
2020-11-23 09:04:18 -0500

    Implementing E-step

---
+++ netflix/naive_em.py
@@ -0,0 +1,72 @@
+"""Mixture model using EM"""
+from typing import Tuple
+import numpy as np
+from common import GaussianMixture
+
+
+
+def estep(X: np.ndarray, mixture: GaussianMixture) -> Tuple[np.ndarray, float]:
+    """E-step: Softly assigns each datapoint to a gaussian component
+
+    Args:
+        X: (n, d) array holding the data
+        mixture: the current gaussian mixture
+
+    Returns:
+        np.ndarray: (n, K) array holding the soft counts
+            for all components for all examples
+        float: log-likelihood of the assignment
+    """
+    n, d = X.shape
+    k, _ = mixture.mu.shape
+    post = np.zeros([n, k])
+    ll = 0
+    p = mixture.p
+    m = mixture.mu
+    v = mixture.var
+
+    for i, xx in enumerate(X):
+        prob = np.zeros(k)
+        for j in range(k):
+            ex = - 1/(2*v[j]) * np.linalg.norm( xx - m[j])**2
+            prob[j] = p[j]/np.power(2 * np.pi*v[j], d/2) * np.exp(ex)
+
+        ll += np.log(prob.sum())
+        prob = prob/prob.sum()
+        post[i,:] = prob
+
+    return post, ll
+    raise NotImplementedError
+
+
+def mstep(X: np.ndarray, post: np.ndarray) -> GaussianMixture:
+    """M-step: Updates the gaussian mixture by maximizing the log-likelihood
+    of the weighted dataset
+
+    Args:
+        X: (n, d) array holding the data
+        post: (n, K) array holding the soft counts
+            for all components for all examples
+
+    Returns:
+        GaussianMixture: the new gaussian mixture
+    """
+    raise NotImplementedError
+
+
+def run(X: np.ndarray, mixture: GaussianMixture,
+        post: np.ndarray) -> Tuple[GaussianMixture, np.ndarray, float]:
+    """Runs the mixture model
+
+    Args:
+        X: (n, d) array holding the data
+        post: (n, K) array holding the soft counts
+            for all components for all examples
+
+    Returns:
+        GaussianMixture: the new gaussian mixture
+        np.ndarray: (n, K) array holding the soft counts
+            for all components for all examples
+        float: log-likelihood of the current assignment
+    """
+    raise NotImplementedError
